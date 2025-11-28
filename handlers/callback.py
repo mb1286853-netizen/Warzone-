f8351baa35ae895b3659c2047af9b31c015fbc19
mb1286853-netizen <mb1286853@gmail.com>
2025-11-28 14:35:16 +0330

    Create callback.py

---
+++ handlers/callback.py
@@ -0,0 +1,9 @@
+from aiogram import Router, types
+from keyboards import main_menu
+
+router = Router()
+
+@router.callback_query(lambda c: c.data == "main_menu")
+async def back_to_main(call: types.CallbackQuery):
+    await call.message.edit_text("منوی اصلی:", reply_markup=main_menu())
+    await call.answer()
