7929034098a9d355d94604320f1cecd819bf5860
mb1286853-netizen <mb1286853@gmail.com>
2025-11-28 14:32:03 +0330

    Create items.py

---
+++ items.py
@@ -0,0 +1,27 @@
+fighters = {
+    "شبحِ شب": {"damage": 220, "armor": 90, "price": 15000},
+    "عقابِ آذرخش": {"damage": 350, "armor": 140, "price": 24000},
+    "سیمرغِ آتشین": {"damage": 520, "armor": 200, "price": 38000},
+    "ققنوسِ سیاه": {"damage": 750, "armor": 280, "price": 58000},
+    "اژدهای زرین": {"damage": 1100, "armor": 400, "price": 90000},
+    "رعدِ آسمان‌شکن": {"damage": 1800, "armor": 600, "price": 150000},
+}
+
+missiles_normal = {
+    "کلاهک ۲ تنی": {"damage": 60, "price_zp": 5000},
+    "کلاهک ۴ تنی": {"damage": 95, "price_zp": 8000},
+    "کلاهک ۶ تنی": {"damage": 135, "price_zp": 12000},
+    "موشک زلزله": {"damage": 190, "price_zp": 18000},
+}
+
+missiles_special = {
+    "شهابِ آتشین": {"damage": 2500, "price_gem": 8},
+    "طوفانِ سیاه": {"damage": 4000, "price_gem": 14},
+    "EMP مغناطیسی": {"price_gem": 20},
+}
+
+missiles_doomsday = {
+    "جحیم": {"damage": 10000, "price_zp": 80000, "price_gem": 10},
+    "نابودگر": {"damage": 16000, "price_zp": 130000, "price_gem": 18},
+    "قیامت": {"damage": 25000, "price_zp": 200000, "price_gem": 25},
+}
